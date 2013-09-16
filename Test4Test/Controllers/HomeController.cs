f7267f74139e461bb221260a84e97121f2813cea
Lucas <metallucas.ds@gmail.com>
2013-09-16 18:12:33 -0300

    Model and Controllers

---
+++ Test4Test/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test4Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "ViewBag do ASP.NET MVC!";
+            ViewData["Mensagem"] = "ViewData do ASP.NET MVC!";
+            TempData["Mensagem"] = "TempData do ASP.NET MVC!";
+
+            return View();
+        }
+
+    }
+}
